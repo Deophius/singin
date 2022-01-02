54ca85d050d424a101e7da3e8da141e74d11f7d0
Deophius <NO1zhuce@163.com>
2022-01-02 22:22:01 +0800

    Wrote report_absent
    It relies on the fact that when a DK is on and someone haven't signed in,
    his OptimisticLockField will be 1.
    This program reads the database for those people's card numbers and prints
    them to the screen, one each line.
    This can be parsed with subprocess.run(text=True, capture_output=True)

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CXX = g++
 CC = gcc
 CXXFLAGS = -Wall -std=c++17 -g -I ./include/
-TARGETS = test_dbman
+TARGETS = test_dbman report_absent
 DLLS = sqlite3mc_x64.dll
 
 .PHONY: all
@@ -11,3 +11,8 @@ test_dbman: test_dbman.o
 	$(CXX) $^ $(DLLS) -o $@
 
 test_dbman.o: test_dbman.cpp dbman.h
+
+report_absent: report_absent.o
+	$(CXX) $^ $(DLLS) -o $@
+
+report_absent.o: report_absent.cpp dbman.h
---
+++ report_absent.cpp
@@ -0,0 +1,14 @@
+#include "dbman.h" 
+
+int main() {
+    Spirit::Connection conn(Spirit::dbname, Spirit::passwd);
+    const std::string query_str = u8"select 卡号 from 上课考勤 \
+    where OptimisticLockField = 1";
+    Spirit::Statement stmt(conn, query_str);
+    while (true) {
+        auto row = stmt.next();
+        if (!row)
+            break;
+        std::cout << row->get<std::string>(0) << '\n';
+    }
+}
