4020ac0f5ab9e41928850c114541e60869abd85a
Deophius <NO1zhuce@163.com>
2022-01-03 14:18:44 +0800

    Adjusted the server to accomodate new IO
    Now you have to pass in the session ID

--- dbman.py
+++ dbman.py
@@ -2,14 +2,16 @@ from json.decoder import JSONDecodeError
 import subprocess, socketserver, time, json
 
 class SpiritTCPHandler(socketserver.StreamRequestHandler):
-    ''' The request handling class for our server 
+    ''' The request handling class for our server
+    In signin.py, sessid is called time
+    In C++ parts, sessid is called dk_curr
     
     It supports two requests, both in JSON format:
-    {"command": "report_absent"}
-        -> {"success": True, "cardnum": ["D1234567", "12345678", ...]} on success
-        -> {"success": False, "what": "error description"} on failure
+    {"command": "report_absent", "sessid": 1}
+        -> {"success": true, "cardnum": ["D1234567", "12345678", ...]} on success
+        -> {"success": false, "what": "error description"} on failure
 
-    {"command": "write_record", "cardnum": ["ABC12345", ...]}
+    {"command": "write_record", "cardnum": ["ABC12345", ...], "sessid":1}
         -> {"success": true} on success
         -> {"success": false, "what": "error description"} on failure
 
@@ -19,7 +21,7 @@ class SpiritTCPHandler(socketserver.StreamRequestHandler):
     If no "command" is found or the contents are not unrecognized, results in:
         {"success": false, "what": "Unknown command!"}
 
-    Assumes that there is no newline in the request
+    Note that Python requires a \n at the end of the request.
     '''
     def write_str(self, s):
         self.wfile.write(bytes(s, 'utf-8'))
@@ -38,10 +40,14 @@ class SpiritTCPHandler(socketserver.StreamRequestHandler):
         if "command" not in req:
             self.write_str('{"success": false, "what": "No command!"}')
             return
+        if 'sessid' not in req:
+            self.write_str('{"success": false, "what": "No session id specified via sessid!"}')
+            return
         if req['command'] == 'report_absent':
             result = {"success": True}
             try:
-                p = subprocess.run(["report_absent"], capture_output=True, text=True)
+                p = subprocess.run(["report_absent"], capture_output=True, text=True, input=str(req['sessid']))
+                assert p.returncode == 0
                 result['cardnum'] = p.stdout.strip('\n').split('\n')
             except:
                 result['what'] = 'Failed to invoke report_absent'
@@ -50,7 +56,8 @@ class SpiritTCPHandler(socketserver.StreamRequestHandler):
         elif req['command'] == 'write_record':
             result = {"success": True}
             try:
-                p = subprocess.run(["write_record"], input = ' '.join(req['cardnum']), text=True)
+                p = subprocess.run(["write_record"], input = str(req['sessid']) + '\n' + ' '.join(req['cardnum']), text=True)
+                assert p.returncode == 0
             except KeyError:
                 result['success'] = False
                 result['what'] = 'request did not include cardnum'
