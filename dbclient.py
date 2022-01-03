a6c1795bd15d0664cefb13a7295d66a17c07c680
Deophius <NO1zhuce@163.com>
2022-01-03 14:21:40 +0800

    Major rewrite of dbclient
    Features:
    1. Compatible with new server and backend
    2. Supports a local card map, for offline work
    3. More tolerable input, allows several tries.

--- dbclient.py
+++ dbclient.py
@@ -6,6 +6,11 @@ import os, sys, socket, json
 host = 'localhost'
 port = 8303
 
+def read_local_cardmap(*params):
+    f = open('stuinfo.json', 'r', encoding='utf-8')
+    resp = json.load(f)
+    return [(s["CardID"], s["StudentName"]) for s in resp["result"]["students"]]
+
 def get_card_map(sess, time, kid):
     ''' Returns a list of (cardnum, name) '''
     if time not in (0, 1, 2):
@@ -26,15 +31,32 @@ def get_card_map(sess, time, kid):
         print(asctime(), ": Bad server reply in get_card_map()", file=sys.stderr)
         sys.exit(1)
 
-def report_absent():
+def report_absent(time):
+    if time not in (0, 1, 2):
+        raise ValueError('time is expected to be 0, 1 or 2')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((host, port))
-        sock.sendall(bytes('{"command": "report_absent"}\n', 'utf-8'))
+        req = {'command': 'report_absent', 'sessid': time}
+        sock.sendall(bytes(json.dumps(req) + '\n', 'utf-8'))
         recv_raw = str(sock.recv(1024), 'utf-8')
     recv_data = json.loads(recv_raw)
     if recv_data['success']:
         return recv_data['cardnum']
     else:
+        print(asctime(), ': dbman server has bad news', file=sys.stderr)
+        print('    ', recv_data['what'], file=sys.stderr)
+        sys.exit(1)
+
+def write_record(time, cardnum):
+    if time not in (0, 1, 2):
+        raise ValueError('time is expected to be 0, 1 or 2')
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        req = {'command': 'write_record', 'sessid': time, 'cardnum': cardnum}
+        sock.connect((host, port))
+        sock.sendall(bytes(json.dumps(req) + '\n', 'utf-8'))
+        recv_raw = str(sock.recv(1024), 'utf-8')
+    recv_data = json.loads(recv_raw)
+    if not recv_data['success']:
         print(asctime(), ': dbman server has bad news', file=sys.stderr)
         print('    ', recv_data['what'], file=sys.stderr)
         sys.exit(1)
@@ -45,9 +67,30 @@ print('User specified:', dk_time)
 print(asctime(), ": POSTing KID")
 kid = signin.get_kid(sess, dk_time)
 print('kid is:', kid)
-card_map = get_card_map(sess, dk_time, kid)
-absent_cards = report_absent()
+print(asctime(), ': Getting card map...')
+
+# FIXME: Change to get_card_map in production release!!
+
+card_map = read_local_cardmap(sess, dk_time, kid)
+print(asctime(), ': Invoking report_absent on server...')
+absent_cards = report_absent(dk_time)
 absent_names = [s[1] for s in card_map if s[0] in absent_cards]
 print('Those people did not DK:')
 for enum, name in enumerate(absent_names):
     print(enum, name)
+
+while True:
+    print('Who needs to sign in? (such as 1 3 4)')
+    try:
+        sign_ids = list(map(int, input().split()))
+    except:
+        print('Sorry, please try again.')
+        continue
+    print('These people will DK:')
+    print(' '.join((absent_names[i] for i in sign_ids)))
+    if os.system('choice /m "Confirm?"') == 1:
+        break
+
+print(asctime(), ': Pushing data to write_record...')
+write_record(dk_time, [absent_cards[i] for i in sign_ids])
+print('Goodbye')
