cf66f12ed8d5d13af92c6c668bf3cae1c1e86b4a
Deophius <NO1zhuce@163.com>
2022-01-16 13:09:55 +0800

    New features in dbman and corresponding tester
    1. Switched to UDP protocol, for broadcasting support
    2. New tell_ip function. When client initiates a tell_ip command, all dbman
    servers on the network will receive it. If this instance has exactly the
    same machine ID, then it will tell the client its IP address. Otherwise,
    it does nothing.
    
    3. Refactored out a new utility function write_object().
    4. Updated version string to 2.1

--- dbman.py
+++ dbman.py
@@ -1,10 +1,13 @@
 from json.decoder import JSONDecodeError
+from platform import machine
 import subprocess, socketserver, time, json, os
 
 # Configuration variable: path to GS.Terminal.Smartboard.exe
 gs_path = 'D:/杭二中/SmartBoardHost 3.4.3.106全包/GS.Terminal.SmartBoard.exe'
+# Configuration variable: the number of the card machine
+machine_id = 'NJ303'
 
-class SpiritTCPHandler(socketserver.StreamRequestHandler):
+class SpiritUDPHandler(socketserver.DatagramRequestHandler):
     ''' The request handling class for our server
     In signin.py, sessid is called time
     In C++ parts, sessid is called dk_curr
@@ -27,6 +30,10 @@ class SpiritTCPHandler(socketserver.StreamRequestHandler):
         -> {"success": true} on success
         -> {"success": false, "what": "error description"} on failure
 
+    {"command": "tell_ip", "machine": "NJ303"} (Look for the machine's IP)
+        -> {"success": true, "ip": "192.168.xxx.xxx"}
+        -> {"success": false, "what": ...} if error occurs
+
     Unrecognized format will result in:
         {"success": false, "what": "Unrecognized format!"}
 
@@ -37,6 +44,9 @@ class SpiritTCPHandler(socketserver.StreamRequestHandler):
     '''
     def write_str(self, s):
         self.wfile.write(bytes(s, 'utf-8'))
+
+    def write_object(self, o):
+        self.wfile.write(bytes(json.dumps(o), 'utf-8'))
 
     def report_absent(self, req):
         if 'sessid' not in req:
@@ -50,7 +60,7 @@ class SpiritTCPHandler(socketserver.StreamRequestHandler):
         except:
             result['what'] = 'Failed to invoke report_absent'
             result['success'] = False
-        self.write_str(json.dumps(result))
+        self.write_object(result)
 
     def write_record(self, req):
         result = {"success": True}
@@ -62,27 +72,44 @@ class SpiritTCPHandler(socketserver.StreamRequestHandler):
             else:
                 raise ValueError
             p = subprocess.run(["write_record"], input = prog_in, text=True)
-            # print(p.returncode)
-            # print(p.stderr)
+            assert p.returncode == 0
         except KeyError as ex:
             result['success'] = False
             result['what'] = 'request did not include ' + ex.args[0]
         except ValueError:
             result['success'] = False
             result['what'] = 'Unknown mode: ' + str(req['mode'])
-        # except:
-        #    result['what'] = 'Failed to invoke write_record'
-        #   result['success'] = False
-        self.write_str(json.dumps(result))
+        except AssertionError:
+            result['success'] = False
+            result['what'] = p.stderr
+        except:
+            result['what'] = 'Failed to invoke write_record'
+            result['success'] = False
+        self.write_object(result)
 
-    def restart_gs(self, req):
+    def restart_gs(self):
         # Ignore return value because this is to ensure GS is closed
         os.system('taskkill /im GS.Terminal.SmartBoard.exe /f /t > NUL 2> NUL')
         if os.system('start cmd /c' + gs_path) == 0:
             self.write_str('{"success": true}')
         else:
             self.write_str('{"success": false", "what": "Failed to start GS.Terminal"}')
-    
+
+    def tell_ip(self, req):
+        try:
+            if req['machine'] == machine_id:
+                # The message is for us
+                from socket import gethostbyname, gethostname
+                self.write_object({
+                    "success": True,
+                    "ip": gethostbyname(gethostname())
+                })
+        except KeyError as ex:
+            self.write_object({
+                "success": False,
+                "what": "No {} specified".format(ex.args[0])
+            })
+
     def handle(self):
         ''' Handles a request'''
         self.data = str(self.rfile.readline().strip(), 'utf-8')
@@ -104,13 +131,16 @@ class SpiritTCPHandler(socketserver.StreamRequestHandler):
                 req['mode'] = 'c'
             self.write_record(req)
         elif req['command'] == 'restart_gs':
-            self.restart_gs(req)
+            self.restart_gs()
+        elif req['command'] == 'tell_ip':
+            self.tell_ip(req)
         else:
             self.write_str('{"success": false, "what": "Unknown command!"}')
 
 if __name__ == '__main__':
-    host = 'localhost'
+    from socket import gethostbyname, gethostname
+    host = gethostbyname(gethostname())
     port = 8303
-    print('dbman version 2.0 listening on port', port)
-    with socketserver.TCPServer((host, port), SpiritTCPHandler) as server:
+    print('dbman version 2.1 listening on port', port)
+    with socketserver.UDPServer((host, port), SpiritUDPHandler) as server:
         server.serve_forever()
--- test_client.py
+++ test_client.py
@@ -1,13 +1,19 @@
 import socket
 
-host = 'localhost'
+# host = socket.gethostbyname(socket.gethostname())
+host = '255.255.255.255'
 port = 8303
 s = input()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((host, port))
-    print('Connected')
-    sock.sendall(bytes(s + '\n', 'utf-8'))
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.settimeout(10)
+    sock.sendto(bytes(s + '\n', 'utf-8'), (host, port))
     print('Sent data')
-    recv = str(sock.recv(1024), 'utf-8')
+    try:
+        recv = str(sock.recv(1024), 'utf-8')
+    except socket.timeout:
+        print('Unable to receive any data!')
+        import sys
+        sys.exit(0)
 print(recv)
