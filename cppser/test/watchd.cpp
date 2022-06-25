7fe2232c6c0d7cc627dce15e05216d361c305a31
Deophius <NO1zhuce@163.com>
2022-06-25 21:07:37 +0800

    Refined watchdog mechanism
    1. Added a hide window command at program launch. This can prevent accidental
    "early deaths".
    2. After the incident on June 25th, maybe we shouldn't exclude "invalid"
    people from sign in.

--- cppser/test/watchd.cpp
+++ cppser/test/watchd.cpp
@@ -2,9 +2,11 @@
 #include <fstream>
 #include <cstdlib>
 #include "../singd.h"
+#include <windows.h>
 
 int main() {
     using namespace Spirit;
+    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
     nlohmann::json config;
     std::ifstream config_file("man.json", std::ios::in);
     config_file >> config;
