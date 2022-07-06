a122030eff8a93297a685e64b10ae71908d812b4
Deophius <NO1zhuce@163.com>
2022-07-06 21:56:02 +0800

    Preparing test_client
    Defaults to the localhost network and decodes the response from server.

--- client/test_client.py
+++ client/test_client.py
@@ -1,7 +1,6 @@
 from socket import *
-import json
 
-addr = (input('Please input host: '), 8303)
+addr = ('127.0.0.1', 8303)
 s = socket(AF_INET, SOCK_DGRAM)
 s.sendto(input().encode('utf-8'), addr)
-print(s.recv(1024))
+print(s.recv(1024).decode())
